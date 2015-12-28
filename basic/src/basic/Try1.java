9b3f7268dd7f859af0c971f74a985f2727045390
Phugelzen Sherpa <phugelzensherpa@Phugelzens-MacBook-Pro.local>
2015-12-27 22:23:22 -0500

    load boss

---
+++ basic/src/basic/Try1.java
@@ -0,0 +1,28 @@
+package basic;
+
+public class Try1 
+	{
+		public static void main(String[] args)
+		{
+		add();
+		System.out.println(6+7);
+		int a=10;
+		System.out.println(a);
+		System.out.println(8);
+		System.out.println("asad");
+		//syntex how to write a variable
+		//datatype variable name=value;
+		int b=12;//local variable//local variable cann't be used in another method.
+		System.out.println(b);
+		int c=a+b;
+		System.out.println(c);
+		System.out.println(a+b);
+		}
+		//syntex:return type method name(){}
+		static void add()
+		{
+		int d=15;
+		System.out.println(d);
+		}
+		
+}
